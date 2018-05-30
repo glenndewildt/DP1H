25302d7b1b1cf4b2860f1604019c83965e883e43
glenndewildt <glenndewildt@gmail.com>
2018-05-30 14:38:28 +0200

    Unit tests logic switches

--- GateTest/UnitTest1.cs
+++ GateTest/UnitTest1.cs
@@ -7,13 +7,117 @@ namespace GateTest
     [TestClass]
     public class Gate
     {
+        //OR logic tests
         [TestMethod]
-        public void AndTest()
+        public void OrTestPositive()
         {
-            AND n = new AND();
-            n. = 1;
+            OR n = new OR();
+            n.input2 = 0;
+            n.input1 = 0;
+            n.Calculate();
+
+            Assert.AreEqual(0, n.Calculate());
+
+        }
+        [TestMethod]
+        public void OrTestNagative1()
+        {
+            OR n = new OR();
+            n.input2 = 1;
+            n.input1 = 0;
+            n.Calculate();
+
+            Assert.AreEqual(1, n.Calculate());
+
+        }
+        [TestMethod]
+        public void OrTestNagative2()
+        {
+            OR n = new OR();
+            n.input2 = 0;
             n.input1 = 1;
             n.Calculate();
+
+            Assert.AreEqual(1, n.Calculate());
+
+        }
+        [TestMethod]
+        public void OrTestNagative3()
+        {
+            OR n = new OR();
+            n.input2 = 1;
+            n.input1 = 1;
+            n.Calculate();
+
+            Assert.AreEqual(1, n.Calculate());
+
+        }
+        //NOT logic tests
+        [TestMethod]
+        public void NotTestPositive()
+        {
+            NOT n = new NOT();
+            n.input1 = 1;
+            n.Calculate();
+
+            Assert.AreEqual(0, n.Calculate());
+
+        }
+        [TestMethod]
+        public void NotTestNagative()
+        {
+            NOT n = new NOT();
+            n.input1 = 0;
+            n.Calculate();
+
+            Assert.AreEqual(1, n.Calculate());
+
+        }
+
+        //AND logic tests
+
+        [TestMethod]
+        public void AndTestPositive()
+        {
+            AND n = new AND();
+            n.input2 = 1;
+            n.input1 = 1;
+            n.Calculate();
+
+            Assert.AreEqual(1, n.Calculate());
+
+        }
+        [TestMethod]
+        public void AndTestNagative1()
+        {
+            AND n = new AND();
+            n.input2 = 0;
+            n.input1 = 0;
+            n.Calculate();
+
+            Assert.AreEqual(0, n.Calculate());
+
+        }
+        [TestMethod]
+        public void AndTestNagative2()
+        {
+            AND n = new AND();
+            n.input2 = 0;
+            n.input1 = 1;
+            n.Calculate();
+
+            Assert.AreEqual(0, n.Calculate());
+
+        }
+        [TestMethod]
+        public void AndTestNagative3()
+        {
+            AND n = new AND();
+            n.input2 = 1;
+            n.input1 = 0;
+            n.Calculate();
+
+            Assert.AreEqual(0, n.Calculate());
 
         }
     }
